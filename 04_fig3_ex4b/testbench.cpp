756cfb0b340f3e3ea6b52edde725a7e41ce738f1
Rishov Sarkar <rishov.sarkar@gatech.edu>
2025-04-10 23:23:34 -0400

    add fig3 ex4b

---
+++ 04_fig3_ex4b/testbench.cpp
@@ -0,0 +1,26 @@
+#include "kernel.hpp"
+#include <stdio.h>
+
+#define DATA_SIZE 2025
+
+int data[MAX_DATA_SIZE];
+
+int main() {
+  int expected = 0;
+  for (int i = 0; i < MAX_DATA_SIZE; i++) {
+    data[i] = (i < DATA_SIZE) ? (i + 1) * 3 : 0;
+    expected += (i < DATA_SIZE) ? i + 1 : 0;
+  }
+
+  int sum_out = 0;
+  int Dropped = 0;
+  kernel(&sum_out, data, Dropped);
+
+  fprintf(stderr, "Expected: <%7d (timing-sensitive)\n", expected);
+  fprintf(stderr, "  Actual:  %7d\n", sum_out);
+  fprintf(stderr, " Dropped:  %7d\n", Dropped);
+
+  // For Vitis HLS to execute C/RTL co-simulation, it requires that the C
+  // testbench return exit code 0.
+  return 0;
+}
